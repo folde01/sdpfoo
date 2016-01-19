0e71396bfae7fce93e0cb42baaf002527101937b
J O <jo6543@yahoo.com>
2016-01-19 19:28:34 +0000

    Initial commit

---
+++ src/com/fooooooooo/Main.java
@@ -0,0 +1,9 @@
+package com.fooooooooo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(args[0] + "hello workd");
+    }
+}
