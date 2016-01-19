2bd21d185a7fafa9f17037b962a5551c36dc4b71
J O <jo6543@yahoo.com>
2016-01-19 20:00:39 +0000

    fblah

---
+++ src/com/fooooooooo/Dog.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jesse on 19/01/16.
+ */
+
+package com.fooooooooo;
+
+public class Dog implements Animal {
+
+    @Override
+    public void speak() {
+        System.out.println("Woof!");
+    }
+}
