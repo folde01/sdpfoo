d284afa0fe9e5d05884963824ba69528e997a621
J O <jo6543@yahoo.com>
2016-01-19 20:01:43 +0000

    moeww!

---
+++ src/com/fooooooooo/Cat.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jesse on 19/01/16.
+ * imagine this class is hard to construct
+ */
+
+package com.fooooooooo;
+
+public class Cat implements Animal {
+    @Override
+    public void speak() {
+        System.out.println("Meow!");
+    }
+}
