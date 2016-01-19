07aaa47cd72e624dba2d5d5bfa10b45f85094489
J O <jo6543@yahoo.com>
2016-01-19 20:03:14 +0000

    just a demo factory from CoP

---
+++ src/com/fooooooooo/AnimalFactory.java
@@ -0,0 +1,21 @@
+/**
+ * Created by jesse on 19/01/16.
+ */
+
+package com.fooooooooo;
+
+public abstract class AnimalFactory {
+
+    public static final int CAT = 0;
+    public static final int DOG = 1;
+
+    public static Animal createAnimal(int type) {
+        switch(type) {
+            case CAT:
+                return new Cat();
+            case DOG:
+                return new Dog();
+        }
+        return null;
+    }
+}
