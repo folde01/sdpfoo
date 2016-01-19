a5ba0589fdf292deaa6f0e1475342e754c74dd19
J O <jo6543@yahoo.com>
2016-01-19 19:58:41 +0000

    finished an example at https://www.youtube.com/watch?v=TBAKKulJTJ4

--- src/com/fooooooooo/Main.java
+++ src/com/fooooooooo/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println(args[0] + "hello workd");
+        Animal animal = AnimalFactory.createAnimal(AnimalFactory.CAT);
+        animal.speak();
     }
 }
